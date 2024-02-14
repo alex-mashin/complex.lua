8d2f7bafc19ec38140adb69f8fe3a1d1de2bee5c
Alexander Mashin <alex.mashin@gmail.com>
2024-02-14 22:21:57 +0700

    Prettify

--- complex.lua
+++ complex.lua
@@ -4,62 +4,68 @@ local sqrt, exp, ln = math.sqrt, math.exp, math.log
 local floor, abs = math.floor, math.abs
 
 complex = {
-    IMAGINARY_CHAR = 'i',
-	PREFIX = false, -- use i / j as a prefix for serialisation.
-	PARENTHESES	= false -- serialise complex numbers in parentheses.
+	IMAGINARY_CHAR	= 'i',
+	PREFIX			= false, -- use i / j as a prefix for serialisation.
+	PARENTHESES		= false -- serialise complex numbers in parentheses.
 }
 
 function complex:new (r, i)
-    self.__index = self
-    return setmetatable (type (r) == 'table' and r or {r = r, i = i or 0}, self)
+	self.__index = self
+	return setmetatable (type (r) == 'table' and r or { r = r, i = i or 0 }, self)
 end
 
 setmetatable (complex, {
-    __call = function (tbl, r, i)
-        return complex:new (r, i)
-    end}
+	__call = function (tbl, r, i)
+		return complex:new (r, i)
+	end }
 )
 
 complex.i = complex (0, 1)
 
 function complex:__unm ()
-    return complex (-self.r, -self.i)
+	return complex (-self.r, -self.i)
 end
 
-function complex:__add (other)
-    return type (self) == 'number'   and complex (self + other.r, other.i)
-        or type (other) == 'number'  and complex (self.r + other, self.i)
-                                     or complex (self.r + other.r, self.i + other.i)
+local function re_im2 (z1, z2)
+	return
+		type (z1) == 'number' and z1 or z1.r,
+		type (z1) == 'number' and 0 or z1.i,
+		type (z2) == 'number' and z2 or z2.r,
+		type (z2) == 'number' and 0 or z2.i
 end
 
-function complex:__sub (other)
-    return self + (-other)
+function complex:__add (addend)
+	local re1, im1, re2, im2 = re_im2 (self, addend)
+	return complex (re1 + re2, im1 + im2)
 end
 
-function complex:__mul (other)
-    return type (self) == 'number'   and complex (self * other.r, self * other.i)
-        or type (other) == 'number'  and complex (self.r * other, self.i * other)
-                                     or complex (self.r * other.r - self.i * other.i, self.i * other.r + self.r * other.i)
+function complex:__sub (subtrahend)
+	return self + (-subtrahend)
+end
+
+function complex:__mul (multiplicand)
+	local re1, im1, re2, im2 = re_im2 (self, multiplicand)
+	return complex (re1 * re2 - im1 * im2, im1 * re2 + re1 * im2)
 end
 
 function complex:conjugate ()
-    return complex (self.r, -self.i)
+	return complex (self.r, -self.i)
 end
 
 function complex:__div (denominator)
-    -- https://www.mesacc.edu/~scotz47781/mat120/notes/complex/dividing/dividing_complex.html
-    local conjugate = type (denominator) == 'number' and complex (denominator) or denominator:conjugate ()
-    local new_numerator, new_denominator = self * conjugate, denominator * conjugate
-    -- new_denominator is real.
-    return complex (new_numerator.r / new_denominator.r, new_numerator.i / new_denominator.r)
+	-- https://www.mesacc.edu/~scotz47781/mat120/notes/complex/dividing/dividing_complex.html
+	local conjugate = type (denominator) == 'number' and complex (denominator) or denominator:conjugate ()
+	local new_numerator, new_denominator = self * conjugate, denominator * conjugate
+	-- new_denominator is real.
+	return complex (new_numerator.r / new_denominator.r, new_numerator.i / new_denominator.r)
 end
 
 function complex:abs2 ()
-    return self.r ^ 2 + self.i ^ 2
+	return self.r ^ 2 + self.i ^ 2
 end
 
 function complex:abs ()
-    return sqrt (self:abs2 ())
+	return sqrt (self:abs2 ())
 end
 
 function complex.polar (abs, arg)
@@ -79,24 +85,25 @@ function complex:arg ()
 end
 
 function complex:exp ()
-    local abs = exp (self.r)
-    -- Special treatment for real numbers to avoid fantom imaginary parts:
-    return self.i == 0 and abs or complex (abs * cos (self.i), abs * sin (self.i))
+	local abs = exp (self.r)
+	-- Special treatment for real numbers to avoid fantom imaginary parts:
+	return self.i == 0 and abs or complex (abs * cos (self.i), abs * sin (self.i))
 end
-    
+	
 function complex:__pow (power)
-    -- Euler:
-    local x = type (self) == 'number' and self or self.i == 0 and self.r > 0 and self.r or nil
-    if x then
-        return complex (power * ln (x)):exp ()
-    else
-    -- de Moivre:
-        local n = type (power) == 'number' and power or power.i == 0 and power.r or nil
-        if n and floor (n) == n then
-            local abs, arg = self:abs (), self:arg ()
-            return complex.polar (abs ^ n, n * arg)
-        end
-    end
+	local re1, im1, re2, im2 = re_im2 (self, power)
+	local x = im1 == 0 and re1 > 0 and re1 or nil
+	if x then
+		-- Euler:
+		return complex (power * ln (x)):exp ()
+	else
+	-- de Moivre:
+		local n = im2 == 0 and re2 or nil
+		if n and floor (n) == n then
+			local abs, arg = self:abs (), self:arg ()
+			return complex.polar (abs ^ n, n * arg)
+		end
+	end
 end
 
 local unpack = unpack or table.unpack
@@ -114,34 +121,34 @@ function complex:roots (n)
 end
 
 function complex:__eq (other)
-    return
-        self.r == other.r and
-        self.i == other.i
+	local re1, im1, re2, im2 = re_im2 (self, other)
+	return re1 == re2 and im1 == im2
 end
 
 function complex:__lt (other)
-    -- incorrect but for sorting:
-    return self.r < other.r or self.r == other.r and self.i < other.i
+	local re1, im1, re2, im2 = re_im2 (self, other)
+	-- incorrect but for sorting:
+	return re1 < re2 or re1 == re2 and im1 < im2
 end
 
 function complex:tostring (i, prefix, parentheses)
-    local im = type (i) == 'string' and i or 'i'
-    local r, i = self.r, self.i
+	local im = type (i) == 'string' and i or 'i'
+	local r, i = self.r, self.i
 
-    local r_str = (r ~= 0 or i == 0) and r or ''
-    local i_str = i ~= 0 and (prefix and im .. abs (i) or (abs (i) == 1 and '' or abs (i)) .. im) or ''
-    local space = (r ~= 0 and i ~= 0) and ' ' or ''
-    local sign = r ~= 0 and i > 0 and '+' or i < 0 and '-' or ''
-    
-    local str = r_str .. space .. sign .. space .. i_str
-    if parentheses then
-        str = '(' .. str .. ')'
-    end
-    return str
+	local r_str = (r ~= 0 or i == 0) and r or ''
+	local i_str = i ~= 0 and (prefix and im .. abs (i) or (abs (i) == 1 and '' or abs (i)) .. im) or ''
+	local space = (r ~= 0 and i ~= 0) and ' ' or ''
+	local sign = r ~= 0 and i > 0 and '+' or i < 0 and '-' or ''
+	
+	local str = r_str .. space .. sign .. space .. i_str
+	if parentheses then
+		str = '(' .. str .. ')'
+	end
+	return str
 end
 
 function complex:__tostring ()
-    return self:tostring (self.IMAGINARY_CHAR, self.PREFIX, self.PARENTHESES)
+	return self:tostring (self.IMAGINARY_CHAR, self.PREFIX, self.PARENTHESES)
 end
 
 return complex
--- test.lua
+++ test.lua
@@ -7,10 +7,6 @@ end or load
 
 local complex = require 'complex'
 
--- complex.IMAGINARY_CHAR = 'j' -- electrical notation.
--- complex.PREFIX = true
--- complex.PARENTHESES = true
-
 local context = {
     complex = complex,
     pi = math.pi,
@@ -31,7 +27,7 @@ local cases = {
     'x', 'y', 'z', 'x:abs ()', 'i', '2 + i',
     'x + y', 'x - y', 'x + 12', 'x - 12', '12 + x', '12 - x',
     'x * y', 'y * 12', '2 * x',
-    'y:conjugate ()', 'x / y', 'y / y', 'x / 5', '5 / x', '5 / x * x',
+    'y:conjugate ()', 'x / y', 'y / y', 'x / 5', '5 / x', '5 / x * x', '10 / z',
     'y:abs ()', 'y:arg () / pi * 180',
     'complex.polar (2, pi / 4)', 'complex.polar (2, 0)', 'complex.polar (2, pi / 2)', 'y:exp ()',
     'x ^ 2', 'z ^ 2', '2 ^ y', '2 ^ x',
